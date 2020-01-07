26da44fee4246a8edb688553ad2ccb50e3965fd6
tsanta94 <tsanta@rabearivelo>
2020-01-07 12:28:32 +0100

    add php

--- reserver.php
+++ reserver.php
@@ -6,7 +6,7 @@
   $j = $_GET["jour"];
   $h = $_GET["horaire"];
   $monfichier = fopen('reservation.csv', 'a');
-  fwrite($monfichier,$l. ',' .$t .','. $j.',' .  $h);
+  fwrite($monfichier,$l. ',' .$t .','. $j.',' .  $h. '\n');
   fclose($monfichier);
   require "projet.php";
   ?>
