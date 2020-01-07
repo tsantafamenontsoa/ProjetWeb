92087f451920f3a11c4f573a779cfa1da86a746e
tsanta94 <tsanta@rabearivelo>
2020-01-07 02:01:34 +0100

    add php

--- projet.php
+++ projet.php
@@ -161,8 +161,7 @@
     //affichage de chaque champ de la ligne en question
     for($i=0; $i<$champs; $i ++)
     {
-    echo $tab[$i] . '</li>'"
-    ";
+    echo $tab[$i] . '</li>';
     }
   }
   ?>
