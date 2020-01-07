7d4af3ffb071709952b4f3ae00f580bfc77eb4d2
tsanta94 <tsanta@rabearivelo>
2020-01-07 03:45:54 +0100

    add php

--- projet.php
+++ projet.php
@@ -176,14 +176,20 @@ $page = (!empty($_GET['page']) ? intval($_GET['page']) : 0);
   }
 
 
+  <a href="?page=<?php echo $page - 1; ?>">Page précédente</a>
+  —
+  <a href="?page=<?php echo $page + 1; ?>">Page suivante</a>
 
-  if($page>0)
-    echo "<a href='?page=". $page - 1. "'>Page précédente</a> --" ;
+  if($page>0) ;
+  ?>
+  <a href="?page=<?php echo $page - 1; ?>">Page précédente</a>
+  —
+<?php
+      if($page<3) ;
+      ?>
+<a href="?page=<?php echo $page + 1; ?>">Page suivante</a>
 
-      if($page<3)
-  echo "  <a href='?page=". $page + 1. "'>Page suivante</a> " ;
 
-?>
 </div>
 	</main>
 	<footer>
