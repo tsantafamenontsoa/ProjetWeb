7aeefb638dbb5e21a097e8d63be36b9feceeeb19
tsanta94 <tsanta@rabearivelo>
2020-01-07 03:55:31 +0100

    add php

--- projet.php
+++ projet.php
@@ -177,11 +177,11 @@ $page = (!empty($_GET['page']) ? intval($_GET['page']) : 0);
 
   if($page>0)
 
-  echo '<a href="?page= ' . intval($page)-1 . '">Page précédente</a> --' ;
+  echo '<a href="?page='.intval($page)-1.'">Page précédente</a> --' ;
 
       if($page<3)
 
-echo ' <a href="?page=' . intval($page)+1 . '">Page suivante</a> ' ;
+echo ' <a href="?page='.intval($page)+1.'">Page suivante</a> ' ;
 ?>
 
 </div>
