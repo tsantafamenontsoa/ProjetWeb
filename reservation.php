285cd29fbf322a8176296036c0ae00d974aedd71
tsanta94 <tsanta@rabearivelo>
2020-01-07 11:29:37 +0100

    add php

--- reservation.php
+++ reservation.php
@@ -181,7 +181,7 @@ $horaires = array();
 }
 
 
-echo "form action='reservation.php' method='get'>"
+echo "form action='reservation.php' method='get'>";
 
 
 
@@ -195,7 +195,7 @@ foreach($list_lieu as $l)
 
 }
 echo '</select>',"\n";
-echo "<input type='submit' value='Choisir'></form>"
+echo "<input type='submit' value='Choisir'></form>";
 
 ?>
 
