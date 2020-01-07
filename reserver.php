eb8cabb18c063eb2f9c183582d8aaa736848fadf
tsanta94 <tsanta@rabearivelo>
2020-01-07 14:07:25 +0100

    add php

--- reserver.php
+++ reserver.php
@@ -9,8 +9,10 @@
   $nbr = $_GET["nombre"];
   $monfichier = fopen('reservation.csv', 'a');
   $tar = array("Plein tarif"=> 15, "Tarif réduit"=>10, "Enfant - Gratuit" =>0);
-  for ($i=0;$i<$nbr;$i++)
-    fwrite($monfichier,$l. ',' .$t .','. $j.',' .  $h. ','.$tarif. ','.$tar[$tarif].', \r\n');
+  for ($i=0;$i<$nbr;$i++){
+    fwrite($monfichier,$l. ',' .$t .','. $j.',' .  $h. ','.$tarif. ','.$tar[$tarif]);
+    fwrite("\r\n");
+  }
   fclose($monfichier);
   require "projet.php";
   ?>
