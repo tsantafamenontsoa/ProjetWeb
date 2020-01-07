176e879c631a96b4b6512ab5563b9c2f00cad9d7
tsanta94 <tsanta@rabearivelo>
2020-01-07 12:14:26 +0100

    add php

--- reserver.php
+++ reserver.php
@@ -8,5 +8,6 @@
   $monfichier = fopen('reservation.csv', 'a+');
   fwrite($monfichier,$l, ",", $t, ",",$j, ",", $h,"\n");
   fclose($monfichier);
+  chmod($file, 0777); 
   require "projet.php";
   ?>
