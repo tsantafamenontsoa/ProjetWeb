598e13ed17156eede09dc3e83cd1569cb25f3310
tsanta94 <tsanta@rabearivelo>
2020-01-07 01:52:50 +0100

    add php

--- projet.php
+++ projet.php
@@ -156,11 +156,12 @@
   while($tab=fgetcsv($fic,1024))
   {
     $champs = count($tab);//nombre de champ dans la ligne en question
-
+    
     //affichage de chaque champ de la ligne en question
     for($i=0; $i<$champs; $i ++)
     {
-      echo '<li>' $tab[$i] '</li>';
+      echo $tab[$i] . "
+      ";
     }
   }
   ?>
